5078813b336e96c81d7e8984db0db9fea28fc0c0
BigupJeff <me@jeffersonreal.uk>
2026-04-13 14:45:05 +0100

    catch invalid nonce error in fetch request

--- src/utils/fetch-http.js
+++ src/utils/fetch-http.js
@@ -7,6 +7,7 @@
  *
  */
 async function fetchHttpRequest( url, options ) {
+
 	try {
 		const response = await fetch( url, {
 			...options,
@@ -18,25 +19,30 @@ async function fetchHttpRequest( url, options ) {
 		if ( !result.ok ) throw result
 
 		return result
+
 	} catch ( error ) {
+
 		if ( error.name === 'TimeoutError' ) {
 			// Request timed out.
-			error.output = [
-				'The request timed out, please try again. If the issue persists, please report to the plugin maintainer.',
-			]
-			error.ok = false
-			console.error( error )
+			error.output = [ 'The request timed out, please try again.' ]
+		} else if ( error.code ) {
+			// WordPress error responses.
+			if ( error.code === 'rest_cookie_invalid_nonce' ) {
+				// invalid nonce.
+				error.output = [ 'Security cookie invalid. Refresh the page and try again.' ]
+			} else {
+				error.output = [ 'WordPress reported: ' + error.message ]
+			}
 		} else if ( !error.output ) {
 			// Likely no server response, so display a generic error to the user.
-			error.output = [
-				'There was a problem communicating with the server, please try again. If the issue persists, please report to the plugin maintainer.',
-			]
-			error.ok = false
-			console.error( error )
+			error.output = [ 'There was a problem communicating with the server, please try again.' ]
 		}
+
 		for ( const message in error.output ) {
 			console.error( error.output[ message ] )
 		}
+
+		error.ok = false
 
 		return error
 	}
